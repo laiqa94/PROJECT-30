5acab7d7678302fe73eae52b993d3586f9342c76
laiqa khan <152205933+laiqa94@users.noreply.github.com>
2024-03-22 02:36:03 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict";
+//Make array of usernames
+const userNames = ['admin', 'Bugs Bunny', 'Daffy Duck', 'Sylvester', 'Tweety'];
+for (let i = 0; i < userNames.length; i++) {
+    if (userNames[i] === 'admin') {
+        console.log('Hello  admin, would you like to see a status report?');
+    }
+    else {
+        console.log(`Hello ${userNames[i]} thank you for logging in again.`);
+    }
+}
